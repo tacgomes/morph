8ea6a052f432000c00394cb6ee2324abe8b7c72b
Lars Wirzenius <lars.wirzenius@codethink.co.uk>
2012-03-05 17:53:57 +0000

    Do not use mbr as the bootloader, since extlinux is sufficient

--- mkimage.sh
+++ mkimage.sh
@@ -78,9 +78,6 @@ sfdisk "$img" <<EOF
 1,,83,*
 EOF
 
-# Install the master boot record boot loader.
-install-mbr "$img"
-
 # Access the partition inside the raw disk image file.
 part=$(dummy_kpartx_add "$img")
 trap "dummy_kpartx_delete $img" EXIT
