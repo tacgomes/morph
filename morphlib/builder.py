5c52cf732a36232a9d20fc70943aefea3b7c9781
Richard Maw <richard.maw@codethink.co.uk>
2012-03-16 11:25:35 +0000

    tar: add -h option to extraction
    This should stop tar from replacing symbolic links with directories
    and instead follow the symbolic links, extracting the files into
    the linked directories

--- morphlib/builder.py
+++ morphlib/builder.py
@@ -494,7 +494,7 @@ class SystemBuilder(BlobBuilder): # pragma: no cover
         with self.build_watch('unpack-strata'):
             for name, filename in self.stage_items:
                 self.msg('unpack %s from %s' % (name, filename))
-                self.ex.runv(['tar', '-C', mount_point, '-xf', filename])
+                self.ex.runv(['tar', '-C', mount_point, '-xhf', filename])
             ldconfig(self.ex, mount_point)
 
     def _create_fstab(self, mount_point):
