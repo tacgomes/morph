87aa2c5a7c1f2cb0ca74959d5a082ccc80f6502a
Richard Maw <richard.maw@codethink.co.uk>
2012-04-24 13:33:07 +0000

    builder2: fix system building
    Accidental indentation caused this function to not be defined in the
    correct scope.

--- morphlib/builder2.py
+++ morphlib/builder2.py
@@ -370,9 +370,10 @@ class SystemBuilder(BuilderBase): # pragma: no cover
                 f.write('append root=/dev/sda1 rootflags=subvol=factory-run '
                                                'init=/sbin/init quiet rw\n')
     
-        def _create_subvolume_snapshot(self, path, source, target):
-            logging.debug('Creating subvolume snapshot %s to %s' % 
-                            (source, target))
+    def _create_subvolume_snapshot(self, path, source, target):
+        logging.debug('Creating subvolume snapshot %s to %s' % 
+                        (source, target))
+        with self.build_watch('create-runtime-snapshot'):
             self.ex.runv(['btrfs', 'subvolume', 'snapshot', source, target],
                          cwd=path)
 
