e6dc394c0f31429b2f54c77b20223651a0ab68ee
Javier Jardón <javier.jardon@codethink.co.uk>
2013-01-22 18:23:39 +0000

    Remove unneded json module imports

--- morphlib/plugins/tarball-systembuilder_plugin.py
+++ morphlib/plugins/tarball-systembuilder_plugin.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2012  Codethink Limited
+# Copyright (C) 2012,2013  Codethink Limited
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,7 +14,6 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 
-import json
 import logging
 import os
 from os.path import relpath
