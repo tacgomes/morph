31b23c4460545bb41d5907524ff56fd272d44ffe
Sam Thursfield <sam.thursfield@codethink.co.uk>
2013-02-06 11:59:15 +0000

    Fix incorrect comment

--- morphlib/source.py
+++ morphlib/source.py
@@ -30,11 +30,6 @@ class Source(object):
     * ``tree`` -- the SHA1 of the tree corresponding to the commit
     * ``morphology`` -- the in-memory representation of the morphology we use
     * ``filename`` -- basename of the morphology filename
-    * ``dependencies`` -- list of Sources for build dependencies for us
-    * ``dependents`` -- list of Source for whom we are a build dependency
-
-    The ``dependencies`` and ``dependents`` lists MUST be modified by
-    the ``add_dependencies`` and ``add_dependent`` methods only.
 
     '''
 
