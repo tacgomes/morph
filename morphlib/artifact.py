529bd21549060c8a8caa1901bf2325fcca079dca
Richard Maw <richard.maw@gmail.com>
2014-07-09 16:15:58 +0000

    Remove get_dependency_prefix_set
    This was used by artifacts to tell what they should put in their path,
    based on what prefixes were used earlier.
    
    This implementation didn't handle recursive deps, and it was trivial to
    open-code it at the call-site, so it is no longer useful.

--- morphlib/artifact.py
+++ morphlib/artifact.py
@@ -41,20 +41,6 @@ class Artifact(object):
     def metadata_basename(self, metadata_name):  # pragma: no cover
         return '%s.%s' % (self.basename(), metadata_name)
 
-    def get_dependency_prefix_set(self):
-        '''Collects all install prefixes of this artifact's build dependencies
-
-           If any of the build dependencies of a chunk artifact are installed
-           to non-standard prefixes, we need to add those prefixes to the
-           PATH of the current artifact.
-
-        '''
-        result = set()
-        for d in self.dependencies:
-            if d.source.morphology['kind'] == 'chunk':
-                result.add(d.source.prefix)
-        return result
-
     def __str__(self):  # pragma: no cover
         return '%s|%s' % (self.source, self.name)
 
