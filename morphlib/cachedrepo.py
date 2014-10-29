4f959335730d37be8d7cd53a2af668d8887e72cc
Sam Thursfield <sam.thursfield@codethink.co.uk>
2014-10-29 16:52:11 +0000

    Remove workaround for an old version of Git

--- morphlib/cachedrepo.py
+++ morphlib/cachedrepo.py
@@ -297,12 +297,8 @@ class CachedRepo(object):
             raise CheckoutError(self, ref, target_dir)
 
     def _update(self):  # pragma: no cover
-        try:
-            morphlib.git.gitcmd(self._runcmd, 'remote', 'update',
-                                'origin', '--prune')
-        except cliapp.AppException, ae:
-            morphlib.git.gitcmd(self._runcmd, 'remote', 'prune', 'origin')
-            morphlib.git.gitcmd(self._runcmd, 'remote', 'update', 'origin')
+        morphlib.git.gitcmd(self._runcmd, 'remote', 'update',
+                            'origin', '--prune')
 
     def __str__(self):  # pragma: no cover
         return self.url
