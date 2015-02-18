f8dad6717ceeafbbbd5e98c9b60626d646e27b90
Sam Thursfield <sam.thursfield@codethink.co.uk>
2015-02-18 11:27:49 +0000

    Update copyright years

--- distbuild/worker_build_scheduler.py
+++ distbuild/worker_build_scheduler.py
@@ -1,6 +1,6 @@
 # distbuild/worker_build_scheduler.py -- schedule worker-builds on workers
 #
-# Copyright (C) 2012, 2014  Codethink Limited
+# Copyright (C) 2012, 2014-2015  Codethink Limited
 # 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
