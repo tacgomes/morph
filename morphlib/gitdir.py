tacgomes/morph

commit 290483010cfc7945cd4483fadd1d98c3b83efb3c

GitDir: Set the fetch or push url when the other is set
Change-Id: I500cb81fd0f133bd9f4e76d46bc0ff8a4f57fe50

morphlib/gitdir.py
@@ -249,12 +249,16 @@ class Remote(object):
         if self.name is not None:
             morphlib.git.gitcmd(self.gd._runcmd, 'remote', 'set-url',
                                 self.name, url)
+        if self.push_url is None:
+            self.set_push_url(url)
 
     def set_push_url(self, url):
         self.push_url = url
         if self.name is not None:
             morphlib.git.gitcmd(self.gd._runcmd, 'remote', 'set-url',
                                 '--push', self.name, url)
+        if self.fetch_url is None:
+            self.set_fetch_url(url)
 
     def _get_remote_url(self, remote_name, kind):
         # As distasteful as it is to parse the output of porcelain

commit 11391bcb949b19bc542f9a46b4392db01b3c7b93

GitDir: Fix setting fetch url when push url is already on-disk
290483010cfc7945cd4483fadd1d98c3b83efb3 broke morph checkout, which
uses set_fetch_url on a repository that has been cloned, hence has its
origin remote url config already on-disk.

The fix prevents it changing the push_url when the fetch_url is set,
unless it is an unnamed remote, as if the config is on-disk, this
already does the right thing.

Change-Id: I6204f664407bab3d7f8ecf0fcca72f5015dee55e

morphlib/gitdir.py
@@ -249,7 +249,7 @@ class Remote(object):
         if self.name is not None:
             morphlib.git.gitcmd(self.gd._runcmd, 'remote', 'set-url',
                                 self.name, url)
-        if self.push_url is None:
+        if self.name is None and self.push_url is None:
             self.set_push_url(url)
 
     def set_push_url(self, url):
@@ -257,7 +257,7 @@ class Remote(object):
         if self.name is not None:
             morphlib.git.gitcmd(self.gd._runcmd, 'remote', 'set-url',
                                 '--push', self.name, url)
-        if self.fetch_url is None:
+        if self.name is None and self.fetch_url is None:
             self.set_fetch_url(url)
 
     def _get_remote_url(self, remote_name, kind):