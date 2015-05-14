tacgomes/morph

commit 4cc75039a78bd8aef9ef464bc0eb6c3ff16809d0

SourceResolver: Allow the resolution of multiple systems
The existing Source resolution code handles resolution of multiple
systems sufficiently.

It is not appropriate to then take this source pool and attempt to
create a build graph from it though, as the logical structure of the
input of what we want to build, and the logical structure of what we
will produce are conflated in the Source object.

If we do not intend to create a build graph from the Source Pool we
generate, then it is an appropriate data structure that may be used to
analyse changes in the input to a build.

Change-Id: If8e4a726f16f8aca000e59ecbbeb7d926cc08391

morphlib/buildcommand.py
@@ -84,7 +84,8 @@ class BuildCommand(object):
         return morphlib.buildenvironment.BuildEnvironment(self.app.settings,
                                                           arch)
 
-    def create_source_pool(self, repo_name, ref, filename, original_ref=None):
+    def create_source_pool(self, repo_name, ref, filename=None,
+                           original_ref=None, filenames=None):
         '''Find the source objects required for building a the given artifact
 
         The SourcePool will contain every stratum and chunk dependency of the
@@ -93,12 +94,14 @@ class BuildCommand(object):
 
         '''
         self.app.status(msg='Creating source pool', chatty=True)
+        if filenames is None and filename is not None:
+            filenames = (filename,)
         srcpool = morphlib.sourceresolver.create_source_pool(
-            self.lrc, self.rrc, repo_name, ref, filename,
+            self.lrc, self.rrc, repo_name, ref, filename=None,
             cachedir=self.app.settings['cachedir'],
             original_ref=original_ref,
             update_repos=not self.app.settings['no-git-update'],
-            status_cb=self.app.status)
+            status_cb=self.app.status, filenames=filenames)
         return srcpool
 
     def validate_sources(self, srcpool):

morphlib/sourceresolver.py
@@ -600,7 +600,7 @@ class SourceResolver(object):
 
 def create_source_pool(lrc, rrc, repo, ref, filename, cachedir,
                        original_ref=None, update_repos=True,
-                       status_cb=None): # pragma: no cover
+                       status_cb=None, filenames=None): # pragma: no cover
     '''Find all the sources involved in building a given system.
 
     Given a system morphology, this function will traverse the tree of stratum
@@ -616,6 +616,8 @@ def create_source_pool(lrc, rrc, repo, ref, filename, cachedir,
 
     '''
     pool = morphlib.sourcepool.SourcePool()
+    if filenames is None and filenames is not None:
+        filenames = (filename,)
 
     def add_to_pool(reponame, ref, filename, absref, tree, morphology):
         sources = morphlib.source.make_sources(reponame, ref,
@@ -634,7 +636,7 @@ def create_source_pool(lrc, rrc, repo, ref, filename, cachedir,
     resolver = SourceResolver(lrc, rrc, tree_cache_manager,
                               buildsystem_cache_manager, update_repos,
                               status_cb)
-    resolver.traverse_morphs(repo, ref, [filename],
+    resolver.traverse_morphs(repo, ref, filenames,
                              visit=add_to_pool,
                              definitions_original_ref=original_ref)
     return pool

commit 1da8ee6f66718de5d5dd413e188425ee4bdcfb47

Fix prototype of create_source_pool()
This fixes an issue in `morph list-artifacts` and probably other commands:

    ERROR: Command failed: morph list-artifacts --quiet file:///ws/master/git.baserock.org/baserock/baserock/definitions HEAD systems/build-system-armv7lhf-jetson.morph systems/weston-system-armv7lhf-jetson.morph systems/genivi-baseline-system-armv7lhf-jetson.morph
    Traceback (most recent call last):
    File "/usr/lib/python2.7/site-packages/cliapp/app.py", line 190, in _run
        self.process_args(args)
    File "/usr/lib/python2.7/site-packages/morphlib/app.py", line 290, in process_args
        cliapp.Application.process_args(self, args)
    File "/usr/lib/python2.7/site-packages/cliapp/app.py", line 539, in process_args
        method(args[1:])
    File "/usr/lib/python2.7/site-packages/morphlib/plugins/list_artifacts_plugin.py", line 67, in list_artifacts
        repo, ref, system_filename)
    File "/usr/lib/python2.7/site-packages/morphlib/plugins/list_artifacts_plugin.py", line 91, in list_artifacts_for_system
        status_cb=self.app.status)
    File "/usr/lib/python2.7/site-packages/morphlib/sourceresolver.py", line 641, in create_source_pool
        definitions_original_ref=original_ref)
    File "/usr/lib/python2.7/site-packages/morphlib/sourceresolver.py", line 583, in traverse_morphs
        definitions_absref, definitions_tree, visit)
    File "/usr/lib/python2.7/site-packages/morphlib/sourceresolver.py", line 408, in _process_definitions_with_children
        definitions_queue = collections.deque(system_filenames)
    TypeError: 'NoneType' object is not iterable

The issue is that the sourceresolver.create_source_pool() function did
not work correctly if passed a single filename. This was a regression from
commit 4cc75039a78bd8aef9ef464bc0eb6c3ff16809d0.

In order to have a logical function prototype, the create_source_pool()
function now only takes a list of systems, instead of taking either a
single system or a list.

morphlib/buildcommand.py
@@ -58,7 +58,7 @@ class BuildCommand(object):
 
         self.app.status(msg='Deciding on task order')
         srcpool = self.create_source_pool(
-            repo_name, ref, filename, original_ref)
+            repo_name, ref, [filename], original_ref)
         self.validate_sources(srcpool)
         root_artifact = self.resolve_artifacts(srcpool)
         self.build_in_order(root_artifact)
@@ -84,8 +84,8 @@ class BuildCommand(object):
         return morphlib.buildenvironment.BuildEnvironment(self.app.settings,
                                                           arch)
 
-    def create_source_pool(self, repo_name, ref, filename=None,
-                           original_ref=None, filenames=None):
+    def create_source_pool(self, repo_name, ref, filenames,
+                           original_ref=None):
         '''Find the source objects required for building a the given artifact
 
         The SourcePool will contain every stratum and chunk dependency of the
@@ -94,14 +94,12 @@ class BuildCommand(object):
 
         '''
         self.app.status(msg='Creating source pool', chatty=True)
-        if filenames is None and filename is not None:
-            filenames = (filename,)
         srcpool = morphlib.sourceresolver.create_source_pool(
-            self.lrc, self.rrc, repo_name, ref, filename=None,
+            self.lrc, self.rrc, repo_name, ref, filenames,
             cachedir=self.app.settings['cachedir'],
             original_ref=original_ref,
             update_repos=not self.app.settings['no-git-update'],
-            status_cb=self.app.status, filenames=filenames)
+            status_cb=self.app.status)
         return srcpool
 
     def validate_sources(self, srcpool):

morphlib/plugins/anchor_plugin.py
@@ -128,7 +128,7 @@ class AnchorPlugin(cliapp.Plugin):
             for system in systems:
                 bc = BuildCommand(self.app)
                 srcpool = bc.create_source_pool(branch_repo, branch_ref,
-                                                system)
+                                                [system])
                 artifact = bc.resolve_artifacts(srcpool)
                 sources = set(a.source for a in artifact.walk())
                 for source in sources:

morphlib/plugins/build_plugin.py
@@ -377,7 +377,7 @@ class BuildPlugin(cliapp.Plugin):
             return
 
         self.app.status(msg='Deciding on task order')
-        srcpool = bc.create_source_pool(repo, commit, system_filename,
+        srcpool = bc.create_source_pool(repo, commit, [system_filename],
                                         original_ref)
         bc.validate_sources(srcpool)
         root = bc.resolve_artifacts(srcpool)

morphlib/plugins/certify_plugin.py
@@ -69,7 +69,7 @@ class CertifyPlugin(cliapp.Plugin):
         self.app.status(
             msg='Creating source pool for %s' % system_filename, chatty=True)
         source_pool = morphlib.sourceresolver.create_source_pool(
-            self.lrc, self.rrc, repo, ref, system_filename,
+            self.lrc, self.rrc, repo, ref, [system_filename],
             cachedir=self.app.settings['cachedir'],
             update_repos = not self.app.settings['no-git-update'],
             status_cb=self.app.status)

morphlib/plugins/cross-bootstrap_plugin.py
@@ -255,7 +255,8 @@ class CrossBootstrapPlugin(cliapp.Plugin):
         build_command = morphlib.buildcommand.BuildCommand(self.app, build_env)
 
         morph_name = morphlib.util.sanitise_morphology_path(system_name)
-        srcpool = build_command.create_source_pool(root_repo, ref, morph_name)
+        srcpool = build_command.create_source_pool(
+            root_repo, ref, [morph_name])
 
         # FIXME: this is a quick fix in order to get it working for
         # Baserock 13 release, it is not a reasonable fix

morphlib/plugins/deploy_plugin.py
@@ -476,7 +476,8 @@ class DeployPlugin(cliapp.Plugin):
         try:
             # Find the artifact to build
             morph = morphlib.util.sanitise_morphology_path(system['morph'])
-            srcpool = build_command.create_source_pool(build_repo, ref, morph)
+            srcpool = build_command.create_source_pool(
+                build_repo, ref, [morph])
 
             artifact = build_command.resolve_artifacts(srcpool)
 

morphlib/plugins/distbuild_plugin.py
@@ -201,7 +201,7 @@ class CalculateBuildGraphPlugin(cliapp.Plugin):
         filename = morphlib.util.sanitise_morphology_path(morph_name)
         build_command = morphlib.buildcommand.BuildCommand(self.app)
         srcpool = build_command.create_source_pool(
-            repo_name, ref, filename, original_ref=original_ref)
+            repo_name, ref, [filename], original_ref=original_ref)
         artifact = build_command.resolve_artifacts(srcpool)
         self.app.output.write(distbuild.encode_artifact(artifact,
                                                         repo_name,
@@ -234,7 +234,7 @@ class WorkerBuild(cliapp.Plugin):
         bc = morphlib.buildcommand.BuildCommand(self.app)
         source_pool = bc.create_source_pool(artifact_reference.repo,
                                             artifact_reference.ref,
-                                            artifact_reference.root_filename)
+                                            [artifact_reference.root_filename])
 
         root = bc.resolve_artifacts(source_pool)
 

morphlib/plugins/graphing_plugin.py
@@ -64,7 +64,7 @@ class GraphingPlugin(cliapp.Plugin):
                                 '%(repo_name)s %(ref)s %(filename)s',
                             repo_name=repo_name, ref=ref, filename=filename)
             builder = morphlib.buildcommand.BuildCommand(self.app)
-            srcpool = builder.create_source_pool(repo_name, ref, filename)
+            srcpool = builder.create_source_pool(repo_name, ref, [filename])
             root_artifact = builder.resolve_artifacts(srcpool)
 
             basename, ext = os.path.splitext(filename)

morphlib/plugins/list_artifacts_plugin.py
@@ -85,7 +85,7 @@ class ListArtifactsPlugin(cliapp.Plugin):
         self.app.status(
             msg='Creating source pool for %s' % system_filename, chatty=True)
         source_pool = morphlib.sourceresolver.create_source_pool(
-            self.lrc, self.rrc, repo, ref, system_filename,
+            self.lrc, self.rrc, repo, ref, [system_filename],
             cachedir=self.app.settings['cachedir'],
             update_repos = not self.app.settings['no-git-update'],
             status_cb=self.app.status)

morphlib/plugins/show_build_log_plugin.py
@@ -63,7 +63,7 @@ class ShowBuildLog(cliapp.Plugin):
 
         build_command = morphlib.buildcommand.BuildCommand(self.app, None)
         srcpool = build_command.create_source_pool(definitions_repo_path, ref,
-                                                   system)
+                                                   [system])
         root = build_command.resolve_artifacts(srcpool)
 
         arch = root.source.morphology['arch']

morphlib/plugins/show_dependencies_plugin.py
@@ -68,7 +68,7 @@ class ShowDependenciesPlugin(cliapp.Plugin):
             self.app.output.write('dependency graph for %s|%s|%s:\n' %
                                   (repo, ref, morph))
 
-            srcpool = build_command.create_source_pool(repo, ref, filename)
+            srcpool = build_command.create_source_pool(repo, ref, [filename])
             root_artifact = build_command.resolve_artifacts(srcpool)
 
             for artifact in reversed(root_artifact.walk()):

morphlib/plugins/system_manifests_plugin.py
@@ -104,7 +104,7 @@ class SystemManifestsPlugin(cliapp.Plugin):
             msg='Creating source pool for %(system)s',
             system=system_filename, chatty=True)
         source_pool = morphlib.sourceresolver.create_source_pool(
-            self.lrc, self.rrc, repo, ref, system_filename,
+            self.lrc, self.rrc, repo, ref, [system_filename],
             cachedir=self.app.settings['cachedir'],
             update_repos = not self.app.settings['no-git-update'],
             status_cb=self.app.status)

morphlib/sourceresolver.py
@@ -598,9 +598,9 @@ class SourceResolver(object):
                 self._resolved_buildsystems)
 
 
-def create_source_pool(lrc, rrc, repo, ref, filename, cachedir,
+def create_source_pool(lrc, rrc, repo, ref, filenames, cachedir,
                        original_ref=None, update_repos=True,
-                       status_cb=None, filenames=None): # pragma: no cover
+                       status_cb=None): # pragma: no cover
     '''Find all the sources involved in building a given system.
 
     Given a system morphology, this function will traverse the tree of stratum
@@ -616,8 +616,6 @@ def create_source_pool(lrc, rrc, repo, ref, filename, cachedir,
 
     '''
     pool = morphlib.sourcepool.SourcePool()
-    if filenames is None and filenames is not None:
-        filenames = (filename,)
 
     def add_to_pool(reponame, ref, filename, absref, tree, morphology):
         sources = morphlib.source.make_sources(reponame, ref,